4a19f3c2c7b2136a034e62585da2bb2c2540db56
David-Moreira <david_nogueira_moreira@hotmail.com>
2021-02-04 20:07:10 +0000

    Exception while parsing expression ')' or ',' expected #478

--- ListWithinList/Program.cs
+++ ListWithinList/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace ListWithinList
 {
@@ -6,7 +8,54 @@ namespace ListWithinList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var test = new Test()
+            {
+                Name = "Testing",
+                Tests = new List<TestList>()
+                {
+                    new TestList()
+                    {
+                        Name = "FirstList",
+                        TestsWithin = new List<TestList>()
+                        {
+                            new TestList()
+                            {
+                                Name = "List Within A List"
+                            }
+                        }
+                    }
+                }
+            };
+
+            ParameterExpression param = Expression.Parameter(typeof(Test), "test");
+
+
+            var thisWorks = "test.Tests.Any(Tests=>Tests.TestsWithin.Any(x=>x.Name==\"List Within A List\"))";
+            var thisDoesNotWork = "test.Tests.Any(Tests=>Tests.TestsWithin.Any(TestsWithin=>TestsWithin.Name==\"List Within A List\"))";
+
+            var lambdaResult = System.Linq.Dynamic.Core.DynamicExpressionParser.ParseLambda(
+                new ParameterExpression[]{ param },
+                null,
+                thisDoesNotWork, test);
+
+            var lambdaDelegate = lambdaResult.Compile();
+            var finalResult = lambdaDelegate.DynamicInvoke(test);
+            Console.WriteLine(finalResult);
+            Console.ReadKey();
+        }
+        
+        public class Test
+        {
+            public string Name { get; set; }
+
+            public List<TestList> Tests { get; set; }
+        }
+
+        public class TestList
+        {
+            public string Name { get; set; }
+
+            public List<TestList> TestsWithin { get; set; }
         }
     }
 }
